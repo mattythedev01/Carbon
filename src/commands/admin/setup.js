22001377cb84af0f7a1944dcd1713f0e1f6be155
mattythedev01 <brennanhiltonrose0103@gmail.com>
2024-10-05 00:17:59 -0400

    Update setup.js

--- src/commands/admin/setup.js
+++ src/commands/admin/setup.js
@@ -5,6 +5,7 @@ const {
   EmbedBuilder,
 } = require("discord.js");
 const moderationSchema = require("../../schemas/moderation");
+const suggestSchema = require("../../schemas/suggestSchema");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -17,27 +18,59 @@ module.exports = {
   run: async (client, interaction) => {
     const { guildId, guild } = interaction;
     let dataGD = await moderationSchema.findOne({ GuildID: guildId });
+    let dataSuggest = await suggestSchema.findOne({ GuildID: guildId });
 
     const setupEmbed = new EmbedBuilder()
-      .setColor("Yellow")
-      .setTitle(`Moderation System Setup Status for ${guild.name}`);
+      .setColor("#FFD700") // A more vibrant yellow color
+      .setTitle(`🚨 Moderation System Setup Status for ${guild.name} 🚨`)
+      .setDescription(
+        "Below is the current setup status for the moderation system in your server."
+      )
+      .setThumbnail(guild.iconURL()) // Adds the server icon as the thumbnail
+      .setTimestamp() // Adds the current timestamp
+      .setFooter({
+        text: "Moderation System Setup",
+        iconURL: client.user.displayAvatarURL(),
+      });
 
     if (!dataGD) {
-      setupEmbed.setDescription("The moderation system is not setup yet.");
+      setupEmbed.addFields({
+        name: "🚫 Moderation System",
+        value: "Not setup",
+        inline: false,
+      });
     } else {
-      setupEmbed.setDescription("The moderation system is setup.");
       setupEmbed.addFields(
         {
-          name: "Multi-guilded",
+          name: "🚫 Moderation System",
+          value: "Correctly Setup",
+          inline: false,
+        },
+        {
+          name: "🔄 Multi-guilded",
           value: `${dataGD.MultiGuilded ? "Yes" : "No"}`,
           inline: true,
         },
         {
-          name: "Logging Channel",
+          name: "🚪 Logging Channel",
           value: `<#${dataGD.LogChannelID}>`,
           inline: true,
         }
       );
+    }
+
+    if (!dataSuggest) {
+      setupEmbed.addFields({
+        name: "🚫 Suggestion Channel",
+        value: "Not setup",
+        inline: true,
+      });
+    } else {
+      setupEmbed.addFields({
+        name: "🚫 Suggestion Channel",
+        value: `<#${dataSuggest.ChannelID}>`,
+        inline: true,
+      });
     }
 
     interaction.reply({ embeds: [setupEmbed], ephemeral: true });
